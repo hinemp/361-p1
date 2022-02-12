6b0567c2babc8069dc8c8e14affc117b34e24c30
Marko Hine <hinemp@dukes.jmu.edu>
2022-02-12 13:29:27 -0500

    lkj

--- valmodel.c
+++ valmodel.c
@@ -105,7 +105,7 @@ ActivateInteger (fsm_t *fsm)
   fsm->buffer = (char *) calloc (100, sizeof (char));
   memset (fsm->buffer, 0, 100 * sizeof (char));
   fsm_t * int_fsm = int_init (fsm->current);
-  fsm->is_val_bad = accept_integer (int_fsm, (int64_t) fsm-> build_int);
+  fsm->is_val_bad = accept_integer (int_fsm, fsm->build_int);
 }
 
 // Use this format string for syntax errors:
