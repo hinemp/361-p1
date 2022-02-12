ae1d50de94de567843cd406ff12c0abb7858a398
Marko Hine <hinemp@dukes.jmu.edu>
2022-02-11 21:47:23 -0500

    lkj

--- intmodel.c
+++ intmodel.c
@@ -115,5 +115,5 @@ MultAndAdd (fsm_t *fsm)
 static void
 SyntaxError (fsm_t *fsm)
 {
-  printf("SYNTAX ERROR: '%c' is not a valid digit", fsm->current[0]);
+  printf("SYNTAX ERROR: '%c' is not a valid digit\n", fsm->current[0]);
 }
