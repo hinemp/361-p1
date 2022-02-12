4462672a9b961959fcd84e7318ae14ac97b88a01
Marko Hine <hinemp@dukes.jmu.edu>
2022-02-12 13:17:17 -0500

    lkj

--- valmodel.c
+++ valmodel.c
@@ -103,7 +103,7 @@ static void
 ActivateInteger (fsm_t *fsm)
 {
   fsm_t * int_fsm = int_init (fsm->current);
-  fsm->is_val_bad = accept_integer (int_fsm, &fsm->build_int);
+  fsm->is_val_bad = accept_integer (int_fsm, (int64_t) &fsm-> build_int);
 }
 
 // Use this format string for syntax errors:
