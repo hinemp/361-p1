9755c037917955d2911ed8f00acb7ae312b395db
Marko Hine <hinemp@dukes.jmu.edu>
2022-02-12 12:53:48 -0500

    lkj

--- tests/public.c
+++ tests/public.c
@@ -92,7 +92,7 @@ END_TEST
 void public_tests (Suite *s)
 {
   TCase *tc_public = tcase_create ("Public");
-  tcase_add_test (tc_public, my_basic_int_test);
+  tcase_add_test (tc_public, my_basic_val_test);
   tcase_add_test (tc_public, PART_string);
   tcase_add_test (tc_public, PART_integer);
   tcase_add_test (tc_public, MIN_value_string);
