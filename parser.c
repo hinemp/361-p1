21a9bfda4d20bf3e403d75834dd743e30f6ab451
Marko Hine <hinemp@dukes.jmu.edu>
2022-02-12 16:18:12 -0500

    lkj

--- parser.c
+++ parser.c
@@ -241,7 +241,7 @@ accept_value (fsm_t *fsm, bool *is_string, char **string, int64_t *value)
     handle_event (fsm, START_STR);
     *string = fsm->buffer;
   } 
-  else
+  if (!*is_string)
   {
     handle_event (fsm, START_INT);
     value = &fsm->build_int;
