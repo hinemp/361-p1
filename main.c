05881479d2df77ae1cd5b1b8300cf85c88cc0129
Marko Hine <hinemp@dukes.jmu.edu>
2022-02-11 21:02:05 -0500

    lkj

--- main.c
+++ main.c
@@ -74,7 +74,7 @@ main (int argc, char **argv)
     fgets (line, 100, (FILE*) fp);
     fclose (fp);
     fsm_t *integer = int_init (line);
-    int64_t value = NULL;
+    int64_t value;
     if (accept_integer (integer, &value)) 
     {
       printf ("INTEGER: '%ld'\n", value);
