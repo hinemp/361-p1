2bda032bf0e792cc08c8018f64ef518941a26073
Marko Hine <hinemp@dukes.jmu.edu>
2022-02-12 19:39:43 -0500

    lkj

--- parser.c
+++ parser.c
@@ -256,7 +256,7 @@ accept_value (fsm_t *fsm, bool *is_string, char **string, int64_t *value)
     else
       handle_event (fsm, BAD_VALUE);
   }
-  if (fsm->is_val_bad)
+  if (fsm->is_val_bad == true)
   {
     *string = NULL;
   }
