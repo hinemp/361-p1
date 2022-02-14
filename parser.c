64b86f24a024f20f0a5849e057996ead17452f99
hinemp <hinemp@dukes.jmu.edu>
2022-02-13 20:46:05 -0500

    asdf

--- parser.c
+++ parser.c
@@ -343,6 +343,7 @@ accept_object (fsm_t *fsm, char **keys)
             handle_event (fsm, WHITESPACE);
             fsm->current++;
           }
+          fsm->current++;
           // First non whitespace char
           if (fsm->current[0] == ',')
           {
